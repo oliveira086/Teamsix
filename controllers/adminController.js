76083c0b3f17959db19394aa962d4d972001683a
ndebianchi <ndebianchi@outlook.com>
2020-04-01 16:07:04 -0300

    update

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,12 +4,13 @@ const fs = require('fs');
 const adminController = {
     index: (req, res) => {
         let caminhoContato = path.join('db', 'contatos.json')
+        
         let lerArquivo = fs.readFileSync(caminhoContato, {encoding: 'utf-8'})
         let listaContatoObjeto = JSON.parse(lerArquivo)
 
         res.render(
           'admin', 
-          { title: 'Admin', listaDeContato:listaContatoObjeto.contatos}
+          { title: 'Admin', listaDeContato:listaContatoObjeto}
         );
       }
 }
